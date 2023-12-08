35b440394b6c8e740bb2eee3276b5914203f87e6
Soner <syln.soner@gmail.com>
2023-12-08 16:24:39 +0300

    Rent a Car project created.

---
+++ src/main/java/com/example/rentacarv1/RentACarV1Application.java
@@ -0,0 +1,13 @@
+package com.example.rentacarv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentACarV1Application.class, args);
+    }
+
+}
